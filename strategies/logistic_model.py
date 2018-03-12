06ba882322b103096b0acdd0c644282fa9e86130
tomato <untrust-worthy@gmx.com>
2018-03-12 15:14:52 +1100

    logistic model

---
+++ strategies/logistic_model.py
@@ -0,0 +1,9 @@
+"""Logistic Model
+    This model intends to use the closing price of previous period
+    as predictor, if the possibility of the prediction exceeds
+    #alpha percent, flag the situtation as tradable."""
+
+import numpy as np
+
+def logi(new, last=None):
+    pass
